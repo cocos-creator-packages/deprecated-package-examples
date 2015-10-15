6abbd5e06821d47ba9e034b938b93fa86be41aff
Johnny Wu <KarasAya@gmail.com>
2015-10-15 13:51:37 +0800

    refine

--- main.js
+++ main.js
@@ -1,81 +1,57 @@
 'use strict';
 
+require('../index');
+
 var Fs = require('fire-fs');
 var Path = require('fire-path');
 
-global.__app = {
-    init: function ( options, cb ) {
-        // initialize ./.settings
-        var settingsPath = Path.join(Editor.appPath, '.settings');
-        Fs.ensureDirSync(settingsPath);
-        Editor.registerProfilePath( 'local', settingsPath );
+Editor.App.extend({
+  init ( options, cb ) {
+    // initialize ./.settings
+    var settingsPath = Path.join(Editor.App.path, '.settings');
+    Fs.ensureDirSync(settingsPath);
+    Editor.registerProfilePath( 'local', settingsPath );
 
-        // TODO: load your profile, and disable packages here
+    // TODO: load your profile, and disable packages here
 
-        Editor.registerPackagePath( Editor.url('app://package-examples/') );
-        Editor.registerPackagePath( Editor.url('app://benchmark/') );
-        Editor.registerPackagePath( Path.join(Editor.appHome, 'packages') );
+    Editor.registerPackagePath( Editor.url('app://package-examples/') );
+    Editor.registerPackagePath( Editor.url('app://benchmark/') );
+    Editor.registerPackagePath( Path.join(Editor.App.home, 'packages') );
 
-        if ( cb ) cb ();
-    },
+    if ( cb ) cb ();
+  },
 
-    run: function () {
-        // create main window
-        var mainWin = new Editor.Window('main', {
-            'title': 'Editor Framework',
-            'width': 900,
-            'height': 700,
-            'min-width': 900,
-            'min-height': 700,
-            'show': false,
-            'resizable': true,
+  run () {
+    // create main window
+    var mainWin = new Editor.Window('main', {
+      'title': 'Editor Framework',
+      'width': 900,
+      'height': 700,
+      'min-width': 900,
+      'min-height': 700,
+      'show': false,
+      'resizable': true,
+    });
+    Editor.mainWindow = mainWin;
+
+    // restore window size and position
+    mainWin.restorePositionAndSize();
+
+    // load and show main window
+    mainWin.show();
+
+    // page-level test case
+    mainWin.load( 'app://package-examples/index.html' );
+
+    // open dev tools if needed
+    if ( Editor.showDevtools ) {
+      // NOTE: open dev-tools before did-finish-load will make it insert an unused <style> in page-level
+      mainWin.nativeWin.webContents.once('did-finish-load', function () {
+        mainWin.openDevTools({
+          detach: true
         });
-        Editor.mainWindow = mainWin;
-
-        // restore window size and position
-        mainWin.restorePositionAndSize();
-
-        // load and show main window
-        mainWin.show();
-
-        // page-level test case
-        mainWin.load( 'app://package-examples/index.html' );
-
-        // open dev tools if needed
-        if ( Editor.showDevtools ) {
-            // NOTE: open dev-tools before did-finish-load will make it insert an unused <style> in page-level
-            mainWin.nativeWin.webContents.once('did-finish-load', function () {
-                mainWin.openDevTools({
-                    detach: true
-                });
-            });
-        }
-        mainWin.focus();
-    },
-
-    load: function () {
-        // TODO
-    },
-
-    unload: function () {
-        // TODO
-    },
-
-    // TODO: try to create a worker
-    // 'app:worker': function () {
-    //     var BrowserWindow = require('browser-window');
-    //     var workerWin = new BrowserWindow({
-    //         show: false,
-    //     });
-
-    //     var Url = require('fire-url');
-    //     var url = Url.format( {
-    //         protocol: 'file',
-    //         pathname: Editor.url('editor-framework://static/worker.html' ),
-    //         slashes: true,
-    //     } );
-    //     workerWin.loadUrl(url);
-    // },
-};
-
-require('../index');
+      });
+    }
+    mainWin.focus();
+  },
+});
